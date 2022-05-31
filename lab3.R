9a2010a6b392f2ac4a7a43fc1e526989c88e62dc
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2022-05-31 15:00:49 +0200

    Add files via upload

--- lab3.R
+++ lab3.R
@@ -6,4 +6,26 @@
 #1/1+e^-beta z
 
 #szansa , szansa, szansa
-krydyt<-read.table(file="kredit.asc",header = T)
+#wczytanie danych
+kredyt<-read.table(file="kredit.asc",header = T)
+
+#a)
+model=glm(kredit~.,data=kredyt,family="binomial")
+
+#b)
+summary(model)
+wsp<-summary(model)$coefficients[,1]
+
+#iloczyn skalarny %*%
+c(1,2) %*% c(10,10)
+pierwszy_klient
+
+prawdopodobienstwa <-function(beta,x){
+  return(1/(1+exp(as.numeric(-1*beta) %*% as.numeric(x))))
+}
+
+
+prawdopodobienstwa(wsp,c(1,kredyt[1,2:ncol(kredyt)]))
+klient1<-kredyt[1,2:ncol(kredyt)]
+
+predict(model,klient1,type="response")
